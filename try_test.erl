f38c867ae736066635b4847d530dab81be32fdca
Siddharth <sid.ravichandran@gmail.com>
2012-08-19 21:31:34 +0530

    Exception handling

--- try_test.erl
+++ try_test.erl
@@ -1,5 +1,5 @@
--module(exception_handling).
--export([demo/0]).
+-module(try_test).
+-compile(export_all).
 
 generate_exception(1) -> a;
 generate_exception(2) -> throw(a);
@@ -10,5 +10,12 @@ generate_exception(5) -> erlang:error(a).
 demo() -> [catcher(I) || I <- [1,2,3,4,5]].
 
 catcher(N) ->
+	try generate_exception(N) of
+		Val -> {N, normal, Val}
+	catch
+		throw:X -> {N, caught, thrown, X};
+		exit:X -> {N, caught, exit, X};
+		error:X -> {N, caught, error, X}
+	end.
 
 
